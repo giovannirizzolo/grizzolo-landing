87ea07249f7413ac6263760f1f3475c296f23be5
giovannirizzolo <rizzolo.giovanni98@gmail.com>
2023-06-28 22:46:33 +0200

    add other sections, fix mobile view

--- components/projects.vue
+++ components/projects.vue
@@ -1,5 +1,31 @@
 <template>
-    <div>
-        
+    <div id="projects" class="md:my-32 py-36 flex flex-col gap-8">
+        <h4 class="md:block hidden max-w-sm">Still figuring out how to compete with Mr. Musk’s SpaceX, but meanwhile...</h4>
+        <h6 class="md:hidden block text-center max-w-sm">Still figuring out how to compete with Mr. Musk’s SpaceX...</h6>
+        <div class="md:hidden block subtitle-2 text-center">
+            ...but meanwhile
+        </div>
+        <div class="px-8 py-16 flex justify-between border border-t-0">
+            <h5 class="hidden md:inline-block">1. Elfi Systems</h5>
+            <div class="text-body-2 inline-block md:hidden">1. Elfi Systems</div>
+            <h5 class="hidden md:inline-block">12/19</h5>
+            <div class="text-body-2 inline-block md:hidden">12/19</div>
+        </div>
+        <div class="px-8 py-16 flex justify-between border border-l-0">
+            <h5 class="hidden md:inline-block">2. NoMafiaMemorial</h5>
+            <div class="text-body-2 inline-block md:hidden">2. NoMafiaMemorial</div>
+            <h5 class="hidden md:inline-block">10/21</h5>
+            <div class="text-body-2 inline-block md:hidden">10/21</div>
+        </div>
+        <div class="px-8 py-16 flex justify-between border border-r-0">
+            <h5 class="hidden md:inline-block">3. Magicmotorsport</h5>
+            <div class="text-body-2 inline-block md:hidden">3. Magicmotorsport</div>
+            <h5 class="hidden md:inline-block">10/22</h5>
+            <div class="text-body-2 inline-block md:hidden">10/22</div>
+        </div>
+        <div class="flex justify-end">
+            <img class="hidden md:inline-block max-w-lg" src="/public/decorative-green.svg" alt="green decorative element evocating a styled lemon">
+            <img class="md:hidden inline-block max-w-lg" src="/public/decorative-green-mobile.svg" alt="green decorative element evocating a styled lemon">
+        </div>
     </div>
 </template>
