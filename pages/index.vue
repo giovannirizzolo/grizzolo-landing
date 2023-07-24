7e1a2ba8758d28c2803438d594d240c9780da1ea
giovannirizzolo <rizzolo.giovanni98@gmail.com>
2023-07-24 22:26:55 +0200

    change images size on mobile

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="flex flex-col xl:flex-row-reverse xl:justify-between min-h-screen justify-start items-center xl:items-stretch mt-8">
-        <nuxt-img class="w-[140px] h-[140px] xl:w-[540px] xl:h-[540px] object-cover col-start-3 rounded-tl-[50px] rounded-br-[50px]" src="/portrait.jpg" alt="grizzolo portrait" id="portrait" format="webp" width="100vw" sizes="xl:100vw lg:100vw md:100vw sm:100vw xs:100vw" />
+        <nuxt-img class="w-[140px] h-[140px] xl:w-[540px] xl:h-[540px] object-cover col-start-3 rounded-tl-[50px] rounded-br-[50px]"
+            src="/portrait.jpg"
+            alt="grizzolo portrait"
+            id="portrait"
+            format="webp"
+            width="100vw"
+            sizes="xl:100vw lg:50vw md:50vw sm:50vw xs:50vw" />
         <div class="flex flex-col gap-4 items-center xl:items-start">
             <div class="text-subtitle-1 hidden xl:block">Welcome to my website</div>
             <div class="flex flex-col gap-4 mb-4">
@@ -12,7 +18,7 @@
                     <h6 class="text-green">developer</h6>
                 </div>
             </div>
-            <iframe style="border-radius: 12px" src="https://open.spotify.com/embed/playlist/1qhdGOWZxofPTMw7KpRTjc?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture; preconnect;" loading="lazy" />
+            <iframe style="border-radius: 12px" src="https://open.spotify.com/embed/playlist/1qhdGOWZxofPTMw7KpRTjc?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture;" loading="lazy" />
             <div class="flex gap-2 flex-col xl:flex-row items-center mt-4">
                 <NuxtLink to="https://calendly.com/grizzolo/30min" target="_blank" class="bg-secondary py-2 px-5 rounded-[10px] text-primary text-caption max-w-xs text-caption xl:text-button">Book a call</NuxtLink>
                 <NuxtLink to="#experiences" class="py-2 px-5 rounded-[10px] border border-secondary text-secondary max-w-xs text-caption xl:text-button">See my experiences</NuxtLink>
